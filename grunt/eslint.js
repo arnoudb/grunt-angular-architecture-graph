47867ddb582a9447fd0820ba05bae530c512c56c
Arnoud Bos <A.J.Bos@rn.rabobank.nl>
2016-11-02 18:55:56 +0100

    combined projects

--- grunt/eslint.js
+++ grunt/eslint.js
@@ -1,11 +1,14 @@
 module.exports = {
-  files: [
-    "grunt/*.js",
-    "tasks/*.js",
-    "test/*.js",
-    "Gruntfile.js"
-  ],
-  options: {
-    config: ".eslintrc"
-  }
+    files: [
+        "src/*.js",
+        "!test-mocks/*.js",
+        "index.js",
+        "grunt/*.js",
+        "tasks/*.js",
+        "test/*.js",
+        "Gruntfile.js"
+    ],
+    options: {
+        config: ".eslintrc"
+    }
 };
