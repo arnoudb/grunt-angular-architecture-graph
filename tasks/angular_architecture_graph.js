01bf816497e0b83a4ce22ae9b58d330b4da26145
Arnoud Bos <A.J.Bos@rn.rabobank.nl>
2016-11-03 19:49:16 +0100

    fixes

--- tasks/angular_architecture_graph.js
+++ tasks/angular_architecture_graph.js
@@ -10,32 +10,41 @@
 
 module.exports = function(grunt) {
 
-  var Helpers           = require("./helpers")(grunt);
+    var Helpers = require("./helpers")(grunt);
 
-  grunt.registerMultiTask("angular_architecture_graph", "Create graphs of your angular projects using angular-architecture-graph.", function() {
-    var options = this.options({
-      hideAngularServices: true,
-      shapeModules: "component",
-      shapeFactories: "ellipse",
-      shapeDirectives: "cds",
-      colorScheme: "paired12"
+    grunt.registerMultiTask("angular_architecture_graph", "Create graphs of your angular projects using angular-architecture-graph.", function() {
+        var options = this.options({
+            hideAngularServices: true,
+            shapeModules: "component",
+            shapeFactories: "ellipse",
+            shapeDirectives: "cds",
+            colorScheme: "paired12"
+        });
+
+
+        var done = this.async();
+
+        Helpers.preprocessTemplates(options);
+
+        this.files.forEach(function(file) {
+            // 1. Parse Files
+            var parsedFiles = Helpers.parseSrcFiles(file);
+
+            // 2. Get codebase graph using angular-architecture graph
+            var codebaseArchitecture = Helpers.analyseFiles(parsedFiles, options);
+
+            //sleep(100)
+
+            setTimeout(function() {
+                // 3. Generate .dot files
+                Helpers.generateGraphFiles(codebaseArchitecture, file);
+
+                // 4. Generate diagram files
+                Helpers.renderDotFiles(file);
+
+           }, 1000);
+        });
+
+        setTimeout(done, 2000);
     });
-
-	Helpers.preprocessTemplates(options);
-
-    this.files.forEach(function (f) {
-      // 1. Parse Files
-      var parsedFiles = Helpers.parseSrcFiles(f);
-
-      // 2. Get codebase graph using angular-architecture graph
-      var codebaseArchitecture = Helpers.analyseFiles(parsedFiles, options);
-
-      // 3. Generate .dot files
-      Helpers.generateGraphFiles(codebaseArchitecture, f);
-
-      // 4. Generate diagram files
-      Helpers.renderDotFiles(f);
-    });
-
-  });
 };
